7027d4727c3eca9384062669b5ba4b279ba48497
tony4834 <116794893+tony4834@users.noreply.github.com>
2022-12-09 16:52:06 +0100

    commit init

--- script/memory.js
+++ script/memory.js
@@ -8,20 +8,10 @@ let nb_alea ;
 let nb_alea_1;
 let nb_alea_2;
 
-let tab_val_case = [];
+let tab_index_case = [];
+
+let case_concernee = document.querySelectorAll(".card");
 
 
-//image 1
-for (i=0; i<2; i++)
-{
-    nb_alea = Math.floor(Math.random()*nb_case);   
-    if (nb_alea in tab_val_case)
-    {
-        //on sort
-    } 
-    else 
-    {
-        tab_val_case[i]=nb_alea;
-    }    
-}
-console.log(tab_val_case);
+/* case_concernee.innerHTML="Fuck" */
+console.log(case_concernee);
